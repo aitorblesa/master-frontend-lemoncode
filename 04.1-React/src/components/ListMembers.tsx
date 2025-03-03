d866c4a7945a9d0c0d7e8a4a39928d6b49268100
Aitor <aitor.ble@gmail.com>
2025-03-03 12:05:53 +0100

    eliminado formulario de pagina de detalle

--- 04.1-React/src/components/ListMembers.tsx
+++ 04.1-React/src/components/ListMembers.tsx
@@ -1,21 +1,54 @@
 import { Link } from "react-router-dom";
-interface MemberEntity {
-  id: string;
-  login: string;
-  avatar: string;
-}
+import { useEffect, useState } from "react";
 
-export function ListMembers({ members }: { members: MemberEntity[] }) {
+import { useMembers } from "../hooks/useMembers";
+
+// interface MemberEntity {
+//   id: string;
+//   login: string;
+//   avatar: string;
+// }
+
+export function ListMembers() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    getMembers({ search });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const [search, setSearch] = useState("lemoncode");
+  const { members, getMembers } = useMembers({ search });
+
+  useEffect(() => {
+    getMembers({ search });
+  }, []);
+
   return (
-    <div className="list">
-      <ul>
-        {members.map(({ id, login, avatar }) => (
-          <li key={id}>
-            <img src={avatar} alt={login} />
-            <Link to={`/detail/${login}`}>{login}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <header>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="Lemoncode"
+            value={search}
+          />
+          <button type="submit">Buscar Miembros</button>
+        </form>
+      </header>
+      <div className="list">
+        <ul>
+          {members.map(({ id, login, avatar }) => (
+            <li key={id}>
+              <img src={avatar} alt={login} />
+              <Link to={`/detail/${login}`}>{login}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
